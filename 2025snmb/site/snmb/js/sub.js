bc6f309e6a2a385b8620f34d254cf5a391acc5bc
belugacurtain <bigunamict@naver.com>
2025-03-12 16:20:03 +0900

    2025 국립서울현충원 전면개편

--- 2025snmb/site/snmb/js/sub.js
+++ 2025snmb/site/snmb/js/sub.js
@@ -31,17 +31,17 @@ function printURL() {
     printWindow.document.write(
         '<!DOCTYPE html>' +
         '<html>' +
-            '<head>' +
-                headHtml +
-            '</head>' +
-            '<body id="body" class="print_body">' +
-                '<div class="sub_head">'+
-                    PrintSubHeadHtml +
-                '</div>'+
-                '<div id="contents">'+
-                    PrintContentsHtml +
-                '</div>'+
-            '</body>' +
+        '<head>' +
+        headHtml +
+        '</head>' +
+        '<body id="body" class="print_body">' +
+        '<div class="sub_head">'+
+        PrintSubHeadHtml +
+        '</div>'+
+        '<div id="contents">'+
+        PrintContentsHtml +
+        '</div>'+
+        '</body>' +
         '</html>'
     );
     printWindow.focus();
@@ -320,29 +320,8 @@ function printURL() {
         });
         //현재 메뉴명 5차일 경우 탭메뉴 UI/UX(와이드) 커스텀 종료
 
-        //컨텐츠 내부 탭메뉴 길이측정 시작
+        //리사이징 시에 컨텐츠 내부 탭메뉴 길이측정 시작
         var $contentsUIUXTab = $('#contents .uiux_tab');
-        $contentsUIUXTab.each(function () {
-            var $this = $(this);
-            var totalItemOuterWidth = 0;
-            for (var i = 0; i < $this.find('.uiux_tab_item').length; i++) {
-                totalItemOuterWidth = totalItemOuterWidth + $this.find('.uiux_tab_item').eq(i).outerWidth();
-            }
-            if ($(this).find('.uiux_tab_list').outerWidth() < totalItemOuterWidth) {
-                $(this).find('.uiux_tab_list').attr('tabindex', '0');
-                    if ($this.find('.uiux_tab_item.active').length) {
-                        var prevTotalWidth = 0;
-                        $this.find('.uiux_tab_item.active').prevAll('.uiux_tab_item').each(function () {
-                            prevTotalWidth += $(this).outerWidth();
-                        });
-                        $(this).find('.uiux_tab_list').animate({
-                            scrollLeft: prevTotalWidth
-                        }, 1);
-                    }
-            } else {
-                $(this).find('.uiux_tab_list').removeAttr('tabindex', '0');
-            }
-        });
         $(window).on('resize', function () {
             $contentsUIUXTab.each(function () {
                 var $this = $(this);
@@ -366,6 +345,32 @@ function printURL() {
                 }
             });
         });
-        //컨텐츠 내부 탭메뉴 길이측정 종료
+        //리사이징 시에 컨텐츠 내부 탭메뉴 길이측정 종료
     });
 })(jQuery);
+//로드시에 컨텐츠 내부 탭메뉴 길이측정 시작
+$(window).on('load', function () {
+    var $contentsUIUXTab = $('#contents .uiux_tab');
+    $contentsUIUXTab.each(function () {
+        var $this = $(this);
+        var totalItemOuterWidth = 0;
+        for (var i = 0; i < $this.find('.uiux_tab_item').length; i++) {
+            totalItemOuterWidth = totalItemOuterWidth + $this.find('.uiux_tab_item').eq(i).outerWidth();
+        }
+        if ($(this).find('.uiux_tab_list').outerWidth() < totalItemOuterWidth) {
+            $(this).find('.uiux_tab_list').attr('tabindex', '0');
+            if ($this.find('.uiux_tab_item.active').length) {
+                var prevTotalWidth = 0;
+                $this.find('.uiux_tab_item.active').prevAll('.uiux_tab_item').each(function () {
+                    prevTotalWidth += $(this).outerWidth();
+                });
+                $(this).find('.uiux_tab_list').animate({
+                    scrollLeft: prevTotalWidth
+                }, 1);
+            }
+        } else {
+            $(this).find('.uiux_tab_list').removeAttr('tabindex', '0');
+        }
+    });
+});
+//로드시에 컨텐츠 내부 탭메뉴 길이측정 종료
